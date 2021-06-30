8454aa2ab4e4261ed803cbc9f0613ec1e8c49eec
Peter <peter@MacBook-Air-de-Peter.local>
2021-06-30 15:58:07 +0200

    impot animal

---
+++ src/components/animal-cards-home/AnimalList.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Animal from './Animal';
+import './AnimalList.css';
+
+function Animallist() {
+
+    const lion = {
+        name: "Simba",
+        species: "lion",
+        age: 2,
+        image:"https://media.tenor.com/images/c6edf1156751130557323cb653d11a49/tenor.gif"
+    }
+    const Pangolin = {
+        name: "Bart",
+        species: "singe",
+        age: 24,
+        image:"https://media.tenor.com/images/c6edf1156751130557323cb653d11a49/tenor.gif"
+    }
+    const Poule = {
+        name: "Grenouille",
+        species: "barbapapa",
+        age: 9,
+        image:"https://media.tenor.com/images/c6edf1156751130557323cb653d11a49/tenor.gif"
+    }
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Animallist;
