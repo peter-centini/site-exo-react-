03eb1357a3bcd5ef111f7faccd9c910cd6b7a236
Peter <peter@MacBook-Air-de-Peter.local>
2021-06-30 15:02:56 +0200

    import logo nav

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../../Assets/logo.png';
+import './Navbar.css';
+
+function navbar() {
+    return (
+        <div className="Navbar">
+            <a href="/"></a><img className="logo" src={logo} alt="logo" /></a>
+            
+        </div>
+    )
+}
+
+export default navbar;
